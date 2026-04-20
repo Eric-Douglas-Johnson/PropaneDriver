6f245a16834e163c5fbb4c2884f3b6cc198eb08a
ericdouglasjohnson32@gmail.com <ericdouglasjohnson32@gmail.com>
2026-04-20 07:51:25 -0500

    removed gallons delivered from RouteSummaryDto and now showing empty string in completion time when there is no route

--- PropaneDriver.Shared/Dtos/RouteSummaryDto.cs
+++ PropaneDriver.Shared/Dtos/RouteSummaryDto.cs
@@ -5,8 +5,6 @@ namespace PropaneDriver.Shared.Dtos
         public DateOnly Date { get; set; }
         public int TotalStops { get; set; }
         public int CompletedStops { get; set; }
-        public double TotalGallonsDelivered { get; set; }
-        public double TotalGallonsRemaining { get; set; }
         public string EstimatedCompletionTime { get; set; } = string.Empty;
         public List<string> Alerts { get; set; } = [];
         public DriverDto? Driver { get; set; }
